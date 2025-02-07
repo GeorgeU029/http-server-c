1ca1e1898063aee826b546fc2301a9de5bfef73e
GeorgeU029 <georgeulloa.fl@gmail.com>
2025-02-07 18:27:42 -0500

    added concurrent connections

--- server.c
+++ server.c
@@ -43,73 +43,85 @@ int main() {
     }
 
     // Listen for incoming connections
-    if (listen(server_fd, 5) != 0) {
+    if (listen(server_fd, 10) != 0) {
         printf("Listen failed: %s \n", strerror(errno));
         return 1;
     }
 
     printf("Waiting for a client to connect...\n");
-
+  
+    signal(SIGCHLD, SIG_IGN);
+    while(1){
+    while (waitpid(-1, NULL, WNOHANG) > 0);
     // Accept a single connection
     int conn = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
     if (conn < 0) {
         printf("Accept failed: %s\n", strerror(errno));
-        return 1;
-    }
-    printf("Client connected\n");
-
-    // Read request
-    char buff[1024] = {0};
-    read(conn, buff, sizeof(buff));
-
-    // Parse request
-    strtok(buff, " ");
-    char *path = strtok(NULL, " ");
-
-    if (path == NULL) {
-        char response[] = "HTTP/1.1 400 Bad Request\r\n\r\n";
-        send(conn, response, strlen(response), 0);
+        continue;
     }
 
-    else if (strncmp(path, "/user-agent", 11) == 0) {
-        char *line = NULL;
-        while ((line = strtok(NULL, "\r\n")) != NULL) {
-            if (strncmp(line, "User-Agent: ", 12) == 0) {
-                char *user_agent = line + 12;  // Skip "User-Agent: "
-                
-                const char *format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
-                
-                char response[1024];
-                snprintf(response, sizeof(response), format, strlen(user_agent), user_agent);
-                send(conn, response, strlen(response), 0);
-                break;
+    if(!fork()){
+        close(server_fd);
+        printf("Client connected\n");
+
+        // Read request
+        char buff[1024] = {0};
+        read(conn, buff, sizeof(buff));
+    
+        // Parse request
+        strtok(buff, " ");
+        char *path = strtok(NULL, " ");
+    
+        if (path == NULL) {
+            char response[] = "HTTP/1.1 400 Bad Request\r\n\r\n";
+            send(conn, response, strlen(response), 0);
+        }
+    
+        else if (strncmp(path, "/user-agent", 11) == 0) {
+            char *line = NULL;
+            while ((line = strtok(NULL, "\r\n")) != NULL) {
+                if (strncmp(line, "User-Agent: ", 12) == 0) {
+                    char *user_agent = line + 12;  // Skip "User-Agent: "
+                    
+                    const char *format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
+                    
+                    char response[1024];
+                    snprintf(response, sizeof(response), format, strlen(user_agent), user_agent);
+                    send(conn, response, strlen(response), 0);
+                    break;
+                }
             }
         }
+    
+        else if (strncmp(path, "/echo/", 6) == 0) {
+            size_t contentLength = strlen(path) - 6;
+            char *content = path + 6;
+    
+            const char *format = "HTTP/1.1 200 OK\r\nContent-Type: "
+                                 "text/plain\r\nContent-Length: %zu\r\n\r\n%s";
+    
+            char response[1024];
+            snprintf(response, sizeof(response), format, contentLength, content);
+            send(conn, response, strlen(response), 0);
+        }
+    
+        else if (strcmp(path, "/") == 0) {
+            char response[] = "HTTP/1.1 200 OK\r\n\r\n";
+            send(conn, response, strlen(response), 0);
+        }
+    
+        else {
+            char response[] = "HTTP/1.1 404 Not Found\r\n\r\n";
+            send(conn, response, strlen(response), 0);
+        }
+        
+        close(conn);
+        exit(0);
     }
-
-    else if (strncmp(path, "/echo/", 6) == 0) {
-        size_t contentLength = strlen(path) - 6;
-        char *content = path + 6;
-
-        const char *format = "HTTP/1.1 200 OK\r\nContent-Type: "
-                             "text/plain\r\nContent-Length: %zu\r\n\r\n%s";
-
-        char response[1024];
-        snprintf(response, sizeof(response), format, contentLength, content);
-        send(conn, response, strlen(response), 0);
-    }
-
-    else if (strcmp(path, "/") == 0) {
-        char response[] = "HTTP/1.1 200 OK\r\n\r\n";
-        send(conn, response, strlen(response), 0);
-    }
-
-    else {
-        char response[] = "HTTP/1.1 404 Not Found\r\n\r\n";
-        send(conn, response, strlen(response), 0);
-    }
-
     close(conn);
-    close(server_fd);
+    }
+   
+
+   
     return 0;
 }
